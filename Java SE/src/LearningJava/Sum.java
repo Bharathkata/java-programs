e2dd5a25f7fd2a6dd1a7b5d0f5293b172901c6a3
Bharathkata <bharathpedure@gmail.com>
2026-07-13 00:25:27 +0530

    Added Sum.java

---
+++ Java SE/src/LearningJava/Sum.java
@@ -0,0 +1,13 @@
+package LearningJava;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int addition = a+b;
+		System.out.println("Sum of 10 and 20 is:" + addition);
+
+	}
+
+}
