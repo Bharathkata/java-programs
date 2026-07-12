43fa59e5174fa7b93c2a2668e0b2492408d6e189
Bharathkata <bharathpedure@gmail.com>
2026-07-13 00:38:11 +0530

    second commit

--- Java SE/src/LearningJava/Sum.java
+++ Java SE/src/LearningJava/Sum.java
@@ -4,9 +4,10 @@ public class Sum {
 
 	public static void main(String[] args) {
 		int a = 10;
-		int b = 20;
-		int addition = a+b;
-		System.out.println("Sum of 10 and 20 is:" + addition);
+		int b = 25;
+		int add = a+b;
+		System.out.println(add);
+		// TODO Auto-generated method stub
 
 	}
 
