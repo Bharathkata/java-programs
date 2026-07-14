10f0f067edd04b72035a6013722a199f9bdf131c
Bharathkata <bharathpedure@gmail.com>
2026-07-14 21:43:52 +0530

    data types

---
+++ Java SE/src/LearningJava/DataTypes.java
@@ -0,0 +1,28 @@
+package LearningJava;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		byte a = 5;
+		short s = 4;
+		long l = 45679055L;
+		int i = 12;
+		float f = 5.4f;
+		double d =  4.55558;
+		char c = '$';
+		boolean Happy = true;
+		System.out.println("Primitive data types in java");
+		System.out.println("Boolean: " + Happy);
+		System.out.println("Byte: " + a);
+		System.out.println("short: " + s);
+		System.out.println("int: " + i);
+		System.out.println("long: " + l);
+		System.out.println("Double: " + d);
+		System.out.println("Char: " + c);
+		
+		
+	
+
+	}
+
+}
