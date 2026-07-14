fa8205f2623275bd5eb887af6d9695751aa5fd26
Bharathkata <bharathpedure@gmail.com>
2026-07-14 21:59:50 +0530

    arithmetic operators

---
+++ Java SE/src/LearningJava/ArithmeticOperators.java
@@ -0,0 +1,18 @@
+package LearningJava;
+import java.util.*;
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         Scanner sc = new Scanner(System.in);
+         int a = sc.nextInt();
+         int b = sc.nextInt();
+         System.out.println("sum:" + (a+b));
+         System.out.println("subtraction:" + (a-b));
+         System.out.println("multiplication:" + (a*b));
+         System.out.println("division:" + (a/b)); 
+         System.out.println("modulus:" + (a%b));
+         
+	}
+
+}
